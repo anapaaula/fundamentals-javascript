3e6d196d70532ebe526c84e549008f6219677408
Ana Paula Santos <ana.paula.santos@ccc.ufcg.edu.br>
2022-03-10 23:17:17 -0300

    Update script.js

--- 02-format-string/script.js
+++ 02-format-string/script.js
@@ -1,19 +1,4 @@
 
-/*const markMass = 78;
-const markHeight = 1.69;
-
-const johnMass = 92;
-const johnHeight = 1.95;
-
-let markBmi = markMass / (markHeight ** 2);
-let johnBmi = johnMass / (johnHeight ** 2);
-
-let markHigherBMI = markBmi > johnBmi;
-
-
-console.log(markBmi, johnBmi);
-console.log(markHigherBMI);*/
-
 const firstName = "Ana";
 const job = "programmer";
 const birthYear = 2001;
