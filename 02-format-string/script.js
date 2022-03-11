b2cb57fcaae99380d19c7129a172936f9a50c871
Ana Paula Santos <ana.paula.santos@ccc.ufcg.edu.br>
2022-03-10 22:26:22 -0300

    Update script.js

--- 02-format-string/script.js
+++ 02-format-string/script.js
@@ -20,5 +20,5 @@ const birthYear = 2001;
 const year = 2022;
 const age = year - birthYear;
 
-const ana = `I'm ${firstName}, a ${age} years old ${job}!`;
+const ana = `I'm ${firstName}, a ${age} year old ${job}!`;
 console.log(ana);
